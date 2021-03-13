91cab13175596fe5ea0c65052428f354923fc755
Samrith Shankar <samrith.298@gmail.com>
2021-03-13 23:20:32 +0100

    test: simple test to check parsing input

--- tests/parse-input.spec.ts
+++ tests/parse-input.spec.ts
@@ -1,5 +1,24 @@
+import { readFileSync } from 'fs';
+
+import { parseInput } from '../src/parse-input';
+
+import { resolveInput } from './test-utils';
+
 describe('parse-input', () => {
     it('should parse the input correctly', () => {
-        expect(true).toBeTruthy();
+        const content = readFileSync(resolveInput('example')).toString();
+        expect(parseInput(content)).toEqual({
+            count: 1,
+            images: [
+                {
+                    size: [3, 4],
+                    bitmap: [
+                        [0, 0, 0, 1],
+                        [0, 0, 1, 1],
+                        [0, 1, 1, 0],
+                    ],
+                },
+            ],
+        });
     });
 });
